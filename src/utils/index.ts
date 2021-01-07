cb6e38dc1ae5f965830b5c99918099f1b2149f97
Ryszard Matula <56454967+rmatu@users.noreply.github.com>
2021-01-08 00:23:11 +0100

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,6 @@ import { arrayCommands, PREFIX } from "../constant";
 export const playSomething = async (song: string, message: any) => {
   const connection = await message.member.voice.channel.join();
 
-  // Create a dispatcher
   const dispatcher = connection.play(
     fs.createReadStream(join(__dirname, `../../src/sound/${song}`))
   );
@@ -18,7 +17,6 @@ export const playSomething = async (song: string, message: any) => {
     console.log(`${song} finished playing!`);
   });
 
-  // Always remember to handle errors appropriately!
   dispatcher.on("error", console.error);
 };
 
